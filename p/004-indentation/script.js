23fa9ce9273b287027d3b3b9ee952771ad8c24b0
ueyuep <danhanstudio@gmail.com>
2023-07-12 12:08:36 +0900

    Clean up indentations

--- p/004-indentation/script.js
+++ p/004-indentation/script.js
@@ -1,19 +1,18 @@
 [].forEach.call(document.querySelectorAll('.code-after'), function($code) {
   var lines = $code.textContent.split('\n');
 
-  if (lines[0] === '')
-  {
-      lines.shift()
+  if (lines[0] === '') {
+    lines.shift()
   }
 
   var matches;
   var indentation = (matches = /^[\s\t]+/.exec(lines[0])) !== null ? matches[0] : null;
   if (!!indentation) {
-      lines = lines.map(function(line) {
-          line = line.replace(indentation, '')
-          return line.replace(/\t/g, '    ')
-      });
+    lines = lines.map(function(line) {
+      line = line.replace(indentation, '')
+      return line.replace(/\t/g, '    ')
+    });
 
-      $code.textContent = lines.join('\n').trim();
+    $code.textContent = lines.join('\n').trim();
   }
 });
