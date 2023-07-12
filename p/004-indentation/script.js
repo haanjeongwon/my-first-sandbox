d1feace475f87248a2f557ad42e9e4d6c0e475bb
ueyuep <danhanstudio@gmail.com>
2023-07-12 15:35:37 +0900

    Rewrite code

--- p/004-indentation/script.js
+++ p/004-indentation/script.js
@@ -1,18 +1,42 @@
-[].forEach.call(document.querySelectorAll('.code-after'), function($code) {
-  var lines = $code.textContent.split('\n');
+// [].forEach.call(document.querySelectorAll('.code-after'), function($code) {
+//   var lines = $code.textContent.split('\n');
+
+//   if (lines[0] === '') {
+//     lines.shift()
+//   }
+
+//   var matches;
+//   var indentation = (matches = /^[\s\t]+/.exec(lines[0])) !== null ? matches[0] : null;
+//   if (!!indentation) {
+//     lines = lines.map(function(line) {
+//       line = line.replace(indentation, '')
+//       return line.replace(/\t/g, '    ')
+//     });
+
+//     $code.textContent = lines.join('\n').trim();
+//   }
+// });
+
+// 출처: https://stackoverflow.com/questions/17257200/removing-leading-whitespace-from-indented-html-source-in-pre-code-tags
+
+const codes = document.querySelectorAll('.code-after');
+
+for (const code of codes) { // forEach로도 해보자
+
+  let lines = code.textContent.split('\n');
 
   if (lines[0] === '') {
     lines.shift()
-  }
+  };
 
-  var matches;
-  var indentation = (matches = /^[\s\t]+/.exec(lines[0])) !== null ? matches[0] : null;
+  let matches;
+  let indentation = (matches = /^[\s\t]+/.exec(lines[0])) !== null ? matches[0] : null;
   if (!!indentation) {
     lines = lines.map(function(line) {
-      line = line.replace(indentation, '')
-      return line.replace(/\t/g, '    ')
+      line = line.replace(indentation, '');
+      return line.replace(/\t/g, '  '); // 탭 문자를 2개의 공백으로 변환하며 반환 -- 그런데 확인해 보니 이미 탭을 공백 2개로 표현하고 있어서 이 줄의 공백 변환은 의미 없음
     });
 
-    $code.textContent = lines.join('\n').trim();
+    code.textContent = lines.join('\n').trim();
   }
-});
+};
