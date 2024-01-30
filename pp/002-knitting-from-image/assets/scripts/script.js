fdd9b4a5c61501f7e0e0c7726f4e5dd1ef9c0402
ueyuep <danhanstudio@gmail.com>
2024-01-30 16:54:10 +0900

    Add touch function for mobile

--- pp/002-knitting-from-image/assets/scripts/script.js
+++ pp/002-knitting-from-image/assets/scripts/script.js
@@ -37,6 +37,28 @@ window.addEventListener('mousemove', function(event) {
   };
 });
 
+window.addEventListener('touchmove', function(event) {
+  // let x = event.offsetX;
+  // let y = event.offsetY;
+  // let pixelData = ctx.getImageData(x, y, 1, 1).data;
+  // let color = 'rgb(' + pixelData[0] + ', ' + pixelData[1] + ', ' + pixelData[2] + ')';
+  console.log(currentX + ', ' + currentY);
+
+  let pixel = ctx.getImageData(currentX, currentY, 1, 1).data;
+  let color = 'rgb(' + pixel[0] + ', ' + pixel[1] + ', ' + pixel[2] + ')';
+
+  let newEl = document.createElement('div');
+  newEl.classList.add('part');
+  newEl.style.backgroundColor = color;
+  overlayLayer.append(newEl);
+
+  currentX += 10;
+  if (currentX >= width) {
+    currentX = 0;
+    currentY += 10;
+  };
+});
+
 window.addEventListener('resize', function() {
   width = image.width;
   height = image.height;
