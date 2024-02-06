d523210b28c7fac35a6d61b705c0ce8379aac55b
ueyuep <danhanstudio@gmail.com>
2024-02-06 22:58:10 +0900

    Fix function for mobile

--- p/009-custom-textarea/script.js
+++ p/009-custom-textarea/script.js
@@ -17,20 +17,23 @@ const close = document.getElementsByClassName('close');
 // };
 
 for (let i = 0; i < wrap.length; i++) {
-  wrap[i].addEventListener('mouseover', function() {
-    wrap[i].classList.add('over');
-  });
-  wrap[i].addEventListener('mouseout', function() {
-    wrap[i].classList.remove('over');
-  });
-  textSize[i].addEventListener('change', function() {
-    textarea[i].style.fontSize = textSize[i].value + 'px';
-  });
   if (isMobile) {
+    wrap[i].addEventListener('touchend', function() {
+      wrap[i].classList.add('over');
+    });
     close[i].addEventListener('click', function() {
       wrap[i].classList.remove('over');
     });
   } else {
+    wrap[i].addEventListener('mouseover', function() {
+      wrap[i].classList.add('over');
+    });
+    wrap[i].addEventListener('mouseout', function() {
+      wrap[i].classList.remove('over');
+    });
     close[i].style.display = 'none';
   };
+  textSize[i].addEventListener('change', function() {
+    textarea[i].style.fontSize = textSize[i].value + 'px';
+  });
 };
